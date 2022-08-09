6decf12d11a2c408f2ccc542499ecc912fcc5daf
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-08-09 11:53:13 -0500

    Create push3.ino

---
+++ Practica1/push3.ino
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+int led1 = 17; 
+int bt1 = 23;
+
+void setup() {
+  pinMode(led1,OUTPUT);
+  pinMode(bt1,INPUT_PULLUP); //configuración pull up internamente
+}
+
+void loop() {
+  
+  if (digitalRead(bt1)==0)
+  {
+    digitalWrite(led1,HIGH);
+  }
+  else digitalWrite(led1,LOW);
+
+}
