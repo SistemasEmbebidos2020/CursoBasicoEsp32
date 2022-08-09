e9fe2d66c065fb16b7aa24de6e699353f58247b2
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-08-09 14:01:33 -0500

    Create pasoapaso1.ino

---
+++ Practica1/pasoapaso1.ino
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+int led1 = 17;
+int led2 = 5;
+int led3 = 18;
+int led4 = 19;
+float t = 3.5;
+int pasos = 0;
+void setup() {
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  pinMode(led4,OUTPUT);
+  
+}
+
+void loop() {
+
+  while(pasos <= 512){
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,LOW);
+  digitalWrite(led4,LOW);
+  delay(t);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,LOW);
+  digitalWrite(led4,LOW);
+  delay(t);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,HIGH);
+  digitalWrite(led4,LOW);
+  delay(t);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,LOW);
+  digitalWrite(led4,HIGH);
+  delay(t);
+  pasos++;
+}
+ 
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,LOW);
+  digitalWrite(led4,LOW);
+
+ 
+}
