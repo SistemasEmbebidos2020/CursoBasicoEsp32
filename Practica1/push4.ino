dda4ec5894d907004275bce908984a51535f36b3
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-08-09 11:55:01 -0500

    Create push4.ino
    configuración pull down internamente

---
+++ Practica1/push4.ino
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+int led1 = 17; 
+int bt1 = 23;
+
+void setup() {
+  pinMode(led1,OUTPUT);
+  pinMode(bt1,INPUT_PULLDOWN); //configuración pull down internamente
+}
+
+void loop() {
+  
+  if (digitalRead(bt1))
+  {
+    digitalWrite(led1,HIGH);
+  }
+  else digitalWrite(led1,LOW);
+
+}
